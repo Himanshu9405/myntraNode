e0798e1e21722182030140428daea424f4ccfe94
piyush-manglani <piyushmanglani45@gmail.com>
2024-11-16 12:03:50 +0530

    completed category api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import morgan from 'morgan'
 import authRoutes from "./routes/authRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js"
 
 // rest object
 const app = express();
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/category',categoryRoutes)
 
 app.listen(PORT, ()=> {
     console.log(`Server stared on ${PORT}`)
